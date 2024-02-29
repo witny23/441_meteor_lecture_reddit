6c1a0ada2fc63db39f9ade2e699ac53128b48bd5
Michael Whitney <whitneym@winthrop.edu>
2024-02-28 20:58:38 -0500

    35a - reply and link challenge

--- reddit/server/main.js
+++ reddit/server/main.js
@@ -1,7 +1,5 @@
 import { Meteor } from 'meteor/meteor';
 import { UP_Collection_Access } from './../imports/api/user_posts.js';
 
-Meteor.startup(() => {
-
-
-});
+/* challenge code ***********************************************************/
+import { Topic_Replies_Collection_Access } from './../imports/api/topic_replies.js';
