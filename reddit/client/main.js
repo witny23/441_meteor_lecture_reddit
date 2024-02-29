2eeacede88b391af45d72e98af19b3d8e5738a38
Michael Whitney <whitneym@winthrop.edu>
2024-02-28 20:30:51 -0500

    18b - challenge 2 - create a topic component with dynamic content

--- reddit/client/main.js
+++ reddit/client/main.js
@@ -10,7 +10,7 @@ import RenderPost from './../imports/ui/RenderPost.js';
 
 const renderPosts =  (passed_posts) =>  {
   let formattedPosts = passed_posts.map((post) => {
-    return <RenderPost key={post._id} />
+    return <RenderPost key={post._id} post_prop_obj={post}/>
     // return (
     //   <p key={post._id}>
     //     {post.topic} have {post.votes} vote[s] {''/* single space before button hack */}
